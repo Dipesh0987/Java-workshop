7c2750fe0620fd0d40608b0f7027ec9973b2ea9e
Dipesh0987 <chhetridipesh91@gmail.com>
2026-01-03 10:52:29 +0545

    updated the class GradeCalculator

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,5 +1,27 @@
 package Workshop;
 
 public class GradeCalculator {
-	
+	public double calculateAverage(double [] grades) {
+		if (grades == null || grades.length == 0) {
+			return 0.0;
+		}
+		double sum = 0;
+		for (double grade:grades) {
+			sum = sum + grade;
+		}
+		return sum / grades.length;
+	}
+	public String getGrade(double average) {
+		if (average >= 90) {
+			return "A";
+		}else if(average >= 80) {
+			return "B";
+		}else if(average >= 70) {
+			return "C";
+		}else if(average >= 60) {
+			return "D";
+		}else {
+			return "F";
+		}
+	}
 }
